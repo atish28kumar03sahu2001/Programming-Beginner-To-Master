e391bfbc1600afb6f4f0c3b308cc777eb2f1e6eb
atishksahuoff2831 <160933920+atishksahuoff2831@users.noreply.github.com>
2024-06-05 05:12:07 +0530

    TypeScript - Part - 09

--- TypeScript/dist/TypeScript.js
+++ TypeScript/dist/TypeScript.js
@@ -167,3 +167,14 @@ const SKS = new Player2(50, 100, 150);
 console.log(`SKS : ${SKS.id}, ${SKS.myHight}, ${SKS.weight}`);
 SKS.ChangeHight = 200;
 console.log(`SKS : ${SKS.id}, ${SKS.myHight}, ${SKS.weight}`);
+class Product1 {
+    constructor(name, price, stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.id = String(Math.random() * 1000);
+        this.getId = () => this.id;
+    }
+}
+const user1 = new Product1("Laptop", 15000, 50);
+console.log(`user1 : ${user1.getId()}, ${user1.name}, ${user1.price}, ${user1.stock}`);
--- TypeScript/src/TypeScript.ts
+++ TypeScript/src/TypeScript.ts
@@ -252,3 +252,24 @@ console.log(`SKS : ${SKS.id}, ${SKS.myHight}, ${SKS.weight}`);
 SKS.ChangeHight = 200;
 console.log(`SKS : ${SKS.id}, ${SKS.myHight}, ${SKS.weight}`);
 
+
+//INTERFACE TRICKS & TIPS
+interface ProductType {
+    name:string;    price:number;
+    stock: number;  
+    offer?:boolean
+}
+interface GiveId {
+    getId: () => string;
+}
+class Product1 implements ProductType, GiveId {
+    private id: string = String(Math.random() * 1000);
+    constructor(
+        public name: string,
+        public price: number,
+        public stock: number,
+    ){}
+    getId = () => this.id;
+}
+const user1 = new Product1("Laptop", 15000, 50);
+console.log(`user1 : ${user1.getId()}, ${user1.name}, ${user1.price}, ${user1.stock}`);
