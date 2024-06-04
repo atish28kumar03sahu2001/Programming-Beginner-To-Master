ae777124cf81ce7aab9123a4576e47595232bb12
atishksahuoff2831 <160933920+atishksahuoff2831@users.noreply.github.com>
2024-06-05 05:08:01 +0530

    TypeScript - Part - 08

--- TypeScript/dist/TypeScript.js
+++ TypeScript/dist/TypeScript.js
@@ -146,3 +146,24 @@ class plater extends Player1 {
 }
 const Voidp = new plater(110, 100, 500, true);
 console.log(`Voidp: ${Voidp.id}, ${Voidp.myHight()}, ${Voidp.weight}, ${Voidp.special}, ${Voidp.getPower()}`);
+//Getter & Setter Function In TypeScript
+class Player2 {
+    constructor(hight, weight, power) {
+        this.hight = hight;
+        this.weight = weight;
+        this.power = power;
+        this.id = String(Math.random() * 1000);
+    }
+    get myHight() {
+        return this.hight;
+    }
+    set ChangeHight(val) {
+        this.hight = val;
+    }
+}
+const AKS = new Player2(15, 30, 45);
+console.log(`AKS : ${AKS.id}, ${AKS.myHight}, ${AKS.weight}`);
+const SKS = new Player2(50, 100, 150);
+console.log(`SKS : ${SKS.id}, ${SKS.myHight}, ${SKS.weight}`);
+SKS.ChangeHight = 200;
+console.log(`SKS : ${SKS.id}, ${SKS.myHight}, ${SKS.weight}`);
--- TypeScript/src/TypeScript.ts
+++ TypeScript/src/TypeScript.ts
@@ -226,3 +226,29 @@ class plater extends Player1 {
 }
 const Voidp = new plater(110,100,500,true);
 console.log(`Voidp: ${Voidp.id}, ${Voidp.myHight()}, ${Voidp.weight}, ${Voidp.special}, ${Voidp.getPower()}`);
+
+//Getter & Setter Function In TypeScript
+class Player2 {
+    public readonly id:string;
+    constructor (
+        private hight: number, 
+        public weight: number, 
+        protected power: number
+    ) {
+        this.id = String(Math.random() * 1000);
+    }
+    get myHight(): number {
+        return this.hight;
+    }
+    set ChangeHight(val: number) {
+        this.hight = val;
+    }
+}
+const AKS = new Player2(15,30,45);
+console.log(`AKS : ${AKS.id}, ${AKS.myHight}, ${AKS.weight}`);
+
+const SKS = new Player2(50,100,150);
+console.log(`SKS : ${SKS.id}, ${SKS.myHight}, ${SKS.weight}`);
+SKS.ChangeHight = 200;
+console.log(`SKS : ${SKS.id}, ${SKS.myHight}, ${SKS.weight}`);
+
