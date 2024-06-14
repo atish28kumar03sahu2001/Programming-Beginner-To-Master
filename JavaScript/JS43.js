b4af5e7805e69e66d66dd71d6657a5eae92644c6
atishksahuoff2831 <160933920+atishksahuoff2831@users.noreply.github.com>
2024-06-15 01:32:35 +0530

    JavaScript Program - 08

---
+++ JavaScript/JS43.js
@@ -0,0 +1,11 @@
+//Switch Statement
+let a = 5;
+switch(a) {
+    case 5 : 
+        console.log("5");
+        break;
+    case 6 : 
+        console.log("6");
+    default : 
+        console.log("Error");
+}
---
+++ JavaScript/JS45.js
@@ -0,0 +1,16 @@
+//Break Statement
+for(let i = 1; i <= 10; i++){
+    if(i === 5){
+        break;
+    }
+    console.log(i);
+}
+console.log("Loop End");
+//Continue Statement
+for(let i = 1; i <= 10; i++){
+    if(i === 5){
+        continue;
+    }
+    console.log(i);
+}
+console.log("Loop End");
