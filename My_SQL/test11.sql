ed51e5c33834838edb9a04217e75da629f5fa0b9
kumar28sahu03atish01 <160933920+kumar28sahu03atish01@users.noreply.github.com>
2024-08-01 12:43:09 +0530

    Final Commit My SQL

--- My_SQL/test11.sql
+++ My_SQL/test11.sql
@@ -1,13 +1,25 @@
 use test1;
-create table userdetail (id bigint, name varchar(30), branch varchar(30));
-insert into userdetail (id, name, branch) values
-(1,"name1","CSE"),(2,"name2","MECH"),(3,"name3","ELEC");
-select * from userdetail;
+create table student (id bigint, sname varchar(50), roll bigint, age bigint, branch varchar(50));
+insert into student (id, sname, roll, age, branch) values 
+(1,"sname1",1,18,"CSE"),(2,"sname2",2,19,"MECH"),(3,"sname3",3,20,"ELEC"),
+(4,"sname4",4,18,"CSE"),(5,"sname5",5,19,"MECH"),(6,"sname6",6,20,"ELEC"),
+(7,"sname7",7,18,"CSE"),(8,"sname8",8,19,"MECH"),(9,"sname9",9,20,"ELEC");
 
-use test1;
-alter table userdetail add column email varchar(50) not null default '';
-describe userdetail;
+select * from student;
 
-insert into userdetail (id, name, branch, email) values
-(4,"name4","EEE","name4@mail.com"),(5,"name5","CHEM","name5@mail.com");
-select * from userdetail;
+alter table student modify age int;
+alter table student add gender varchar(10);
+insert into student (id, sname, roll, age, branch, gender) values 
+(10,"sname10",10,18,"CSE","male"),(11,"sname11",11,19,"MECH","female"),(3,"sname3",3,20,"ELEC","male");
+alter table student change age sage int;
+alter table student modify roll bigint after sage;
+alter table student drop column gender;
+alter table student rename studb;
+select * from studb;
+
+-- describe command will describe about table details
+describe student;
+
+-- Drop command used for to delete the whole table. Truncate command used to delete all data of the table
+truncate studb;
+drop table studb;
